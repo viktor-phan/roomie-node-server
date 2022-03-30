6739998586ed0f9d7b6b98deb6ba544631b85595
viktor-phan <viktor.thang.phan@gmail.com>
2022-03-29 20:18:56 -0400

    BEN-0001 Set up User endpoints

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
-import express from "express";
 import Example from "./Example.js";
 import HouseRouter from "./House.js";
+import UserRouter from "./User.js";
+
 export default (app) => {
   app.use(Example);
   //more routes add below
   app.use(HouseRouter);
+  app.use(UserRouter);
 };
