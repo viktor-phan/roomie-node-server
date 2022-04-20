d4a0a5fd46554b42f7c5bedfde5ae1db340b14b7
viktor-phan <viktor.thang.phan@gmail.com>
2022-04-20 13:36:57 -0400

    Added list endpoints of API in console

--- src/index.js
+++ src/index.js
@@ -4,17 +4,18 @@ import routes from "./routes/index.js";
 import connectDatabase from "./database/index.js";
 import dotenv from "dotenv";
 import cors from "cors";
+import listEndpoints from "express-list-endpoints";
 
-const result = dotenv.config({silent: true});
-console.log(result.error)
+const result = dotenv.config({ silent: true });
+console.log(result.error);
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 app.use(bodyParser.json());
 routes(app);
 const port = process.env.PORT || 5000;
-
 app.listen(port, async () => {
   await connectDatabase();
   console.log(`Server running at port ${port} localhost!`);
 });
+console.log(listEndpoints(app));
